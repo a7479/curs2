ddc67a434769cc569b48a4e5d32d20a2d75c390d
Александр <perchatki79@gmail.com>
2023-05-31 22:00:40 +0300

    Initial commit

---
+++ src/test/java/sky/pro/curs2/Curs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.curs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
